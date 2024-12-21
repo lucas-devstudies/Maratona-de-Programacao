ef85df297c8775553e9869e650f6aedabad3162b
Lucas Ferreira da Silva <87401472+lucas-devstudies@users.noreply.github.com>
2024-12-20 21:11:56 -0400

    Create medalhas.cpp

---
+++ medalhas.cpp
@@ -0,0 +1,100 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int t1,t2,t3;
+    cin>>t1>>t2>>t3;
+    if (t1< t2)
+    {
+        if (t1<t3)
+        {
+            cout<<1<<endl;
+            if (t2<t3)
+            {
+                cout<<2<<endl;
+                cout<<3<<endl;
+            }
+            else
+            {
+                cout<<3<<endl;
+                cout<<2<<endl;
+            }
+        }
+        else{
+            cout<<3<<endl;
+            if (t1<t2)
+            {
+                cout<<1<<endl;
+                cout<<2<<endl;
+            }
+            else
+            {
+                cout<<2<<endl;
+                cout<<1<<endl;
+            }
+        }
+        
+    }
+    else if(t2<t3)
+    {
+        if (t2<t1)
+        {
+            cout<<2<<endl;
+            if (t1<t3)
+            {
+                cout<<1<<endl;
+                cout<<3<<endl;
+            }
+            else
+            {
+                cout<<3<<endl;
+                cout<<1<<endl;
+            }
+        }
+        else{
+            cout<<1<<endl;
+            if (t2<t3)
+            {
+                cout<<2<<endl;
+                cout<<3<<endl;
+            }
+            else
+            {
+                cout<<3<<endl;
+                cout<<2<<endl;
+            }
+        }
+    }
+    else
+    {
+        if (t3<t1)
+        {
+            cout<<3<<endl;
+            if (t1<t2)
+            {
+                cout<<1<<endl;
+                cout<<2<<endl;
+            }
+            else
+            {
+                cout<<2<<endl;
+                cout<<1<<endl;
+            }
+        }
+        else
+        {
+            cout<<1<<endl;
+            if (t2<t3)
+            {
+                cout<<2<<endl;
+                cout<<3<<endl;
+            }
+            else
+            {
+                cout<<3<<endl;
+                cout<<2<<endl;
+            }
+        }
+    }
+    return 0;
+}
