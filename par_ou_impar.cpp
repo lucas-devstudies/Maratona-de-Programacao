54ef4ab3a5ae7318a42b5db1c2a25f1313a2391e
Lucas Ferreira da Silva <87401472+lucas-devstudies@users.noreply.github.com>
2024-12-20 21:08:27 -0400

    Create par_ou_impar.cpp

---
+++ par_ou_impar.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a,m;
+    cin>>a>>m;
+    if ((a+m)%2==0)
+    {
+        cout<<"Bino";
+    }
+    else
+    {
+        cout<<"Cino"<<endl;
+    }
+    return 0;
+}
