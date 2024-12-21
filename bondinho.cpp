ec1ad101f1d5db67b485399215b1eed8b01d4d7d
Lucas Ferreira da Silva <87401472+lucas-devstudies@users.noreply.github.com>
2024-12-20 21:07:49 -0400

    Create bondinho.cpp

---
+++ bondinho.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a,m,t;
+    cin>>a>>m;
+    if (a+m <= 50)
+    {
+        cout<<'S';
+    }
+    else
+    {
+        cout<<'N';
+    }
+    return 0;
+}
