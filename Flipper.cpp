f36f4f1bffc9ccfa0b58bc3799d81b59a628ecdb
Lucas Ferreira da Silva <87401472+lucas-devstudies@users.noreply.github.com>
2024-12-20 21:04:40 -0400

    Create Flipper.cpp

---
+++ Flipper.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n,m;
+    cin>>n>>m;
+    if (n==0){
+        cout<<'C';
+    }
+    else
+    {
+        if(m==1)
+        {
+            cout<<'A'<<endl;
+        }
+        else
+        {
+            cout<<'B'<<endl;
+        }
+    }
+}
