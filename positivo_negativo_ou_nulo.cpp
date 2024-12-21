a73d9cddf52f6f7b053f5f617a492d59800e964d
Lucas Ferreira da Silva <87401472+lucas-devstudies@users.noreply.github.com>
2024-12-20 21:07:15 -0400

    Create positivo_negativo_ou_nulo.cpp

---
+++ positivo_negativo_ou_nulo.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    if (n==0)
+    {
+        cout<<"nulo";
+    }
+    else if (n>0)
+    {
+        cout<<"positivo";
+    }
+    else
+    {
+        cout<<"negativo";
+    }
+    return 0;
+}
