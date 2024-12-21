65c50a2e7bbf7457e05e1e06f0b7e64914a78f66
Lucas Ferreira da Silva <87401472+lucas-devstudies@users.noreply.github.com>
2024-12-20 20:59:18 -0400

    Create media_inteira.cpp

---
+++ media_inteira.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    // Lendo a entrada do exercício
+    int A, B,soma;
+    cin >> A >> B;
+    soma = (A+B)/2;
+    cout<<soma;
+    
+    return 0;
+}
