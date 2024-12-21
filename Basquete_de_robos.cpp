58ddd9166e2a0947efa2b23ac5ec3615337ea795
Lucas Ferreira da Silva <87401472+lucas-devstudies@users.noreply.github.com>
2024-12-20 21:13:16 -0400

    Create Basquete_de_robos.cpp

---
+++ Basquete_de_robos.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int d;
+    cin>>d;
+    if (d<=800)
+    {
+        cout<<1;
+    }
+    else if(d<=1400)
+    {
+        cout<<2;
+    }
+    else
+    {
+        cout<<3;
+    }
+    return 0;
+}
